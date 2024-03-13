dfa4b2be819d5a954a1127c678fecbbeb15f24db
Javito <fj.diazb@gmail.com>
2024-03-13 21:13:12 +0100

    uno

--- app/view3/view3.js
+++ app/view3/view3.js
@@ -11,7 +11,13 @@ app.controller('View3Ctrl', function ($scope, $http) {
 
 
     $scope.paises = []
-    //$scope.paisSeleccionado="0"
+    $scope.paisSeleccionado="0"
+    $scope.nombreComun=""
+    $scope.nombreOficial=""
+    $scope.capital=""
+    $scope.poblacion=""
+    $scope.bandera=""
+    $scope.escudo=""
 
 
     $scope.cargarPaises = url => {
@@ -26,6 +32,21 @@ app.controller('View3Ctrl', function ($scope, $http) {
             $scope.paises=datos.data
         })
 
+    }
+    $scope.cargaPais=()=>{
+        var configPais = {
+            url:`https://restcountries.com/v3.1/name/${$scope.paisSeleccionado}`
+        }
+          
+        var response=$http(configPais).then(datos=>{
+            $scope.nombreComun=datos.data[0].name.common
+            $scope.nombreOficial=datos.data[0].name.official
+            $scope.capital=datos.data[0].capital
+            $scope.poblacion=datos.data[0].population
+            $scope.bandera=datos.data[0].flags.png
+            $scope.escudo=datos.data[0].coatOfArms.png
+            
+        })
     }
 
 })
