b996e26aae50775b680d43563893e5ec3ef6424e
k.perez_ext <k.perez_ext@navblue.aero>
2024-03-14 17:15:00 +0100

    View 4 select

---
+++ app/view4/view4.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var app = angular.module('myApp.view4', ['ngRoute'])
+
+
+app.controller('View4Ctrl', function ($scope, $http) {
+
+    $scope.cargarPaises = url => {
+
+        var config = {
+            url: url
+        }
+
+        $http(config).then(function (datos) {
+
+            console.log(datos.data)
+            $scope.paises=datos.data
+        })
+
+    }
+
+    $scope.$watch('paisSeleccionado', function(valor) {
+        if (valor) {
+            $scope.datosDelPais = $scope.paises.find(pais => pais.name.common === valor);
+        } else {
+            $scope.datosDelPais = null;
+        }
+    });
+})
