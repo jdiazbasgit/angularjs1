0ad11566adefd19dcb5ad3de7c2192b5b7f95c14
k.perez_ext <k.perez_ext@navblue.aero>
2024-03-19 08:57:03 +0100

    Adding select with table

--- app/view4/view4.js
+++ app/view4/view4.js
@@ -6,7 +6,14 @@ var app = angular.module('myApp.view4', ['ngRoute'])
 app.controller('View4Ctrl', function ($scope, $http) {
 
     $scope.paises = []
-    $scope.datosDelPais = null;
+    $scope.paisSeleccionado="0"
+    $scope.nombreComun=""
+    $scope.nombreOficial=""
+    $scope.capital=""
+    $scope.poblacion=""
+    $scope.bandera=""
+    $scope.escudo=""
+    //$scope.datosDelPais = null;
 
     $scope.cargarPaises = url => {
 
@@ -22,12 +29,28 @@ app.controller('View4Ctrl', function ($scope, $http) {
 
     }
 
-    $scope.$watch('paisSeleccionado', function(valor) {
+    $scope.cargaPais=()=>{
+        var configPais = {
+            url:`https://restcountries.com/v3.1/name/${$scope.paisSeleccionado}`
+        }
+          
+        var response=$http(configPais).then(datos=>{
+            $scope.nombreComun=datos.data[0].name.common
+            $scope.nombreOficial=datos.data[0].name.official
+            $scope.capital=datos.data[0].capital
+            $scope.poblacion=datos.data[0].population
+            $scope.bandera=datos.data[0].flags.png
+            $scope.escudo=datos.data[0].coatOfArms.png
+            
+        })
+    }
+
+    /*$scope.$watch('paisSeleccionado', function(valor) {
         if (valor) {
             $scope.datosDelPais = $scope.paises.find(pais => pais.name.common === valor);
         } else {
             $scope.datosDelPais = null;
         }
-    });
+    });*/
 
 })
