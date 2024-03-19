90cf27eed6cedf9ebdad39aa41ff8b3cc94854ff
jdiazbasgit <git@studiotapal.es>
2024-03-19 16:08:10 +0100

    rootScope

--- platforms/browser/www/app.js
+++ platforms/browser/www/app.js
@@ -7,7 +7,7 @@ angular.module('myApp', [
   'myApp.view2',
   'myApp.view3',
   'myApp.view4',
-  'myApp.generales',
+
   'myApp.version'
 ]).
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider,$scope) {
